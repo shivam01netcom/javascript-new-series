e85bc844c453e7f4040af83c4f676d46e8ba4e04
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-23 08:18:22 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+const name = "shivam"
+const rollNumber = 123
+const result = true
+
+const largeNumber = 76765643457680976n;
+const largest = BigInt(7456789087654345);
+
+
+console.log(typeof name);
+console.log(typeof rollNumber);
+console.log(typeof result);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol); // function
+console.log(typeof BigInt); // function
+console.log(typeof largeNumber); //bigint
+console.log(typeof largest); //bigint
+
+/*
+Two type of dataTypes in JS
+1. primitive dataType( ssbbnnu)
+(
+    string
+    symbol
+    bigint
+    boolean
+    number
+    null 
+    undefined
+)
+2. NonPrimitive dataTypes
+(
+    array
+    object
+)
+*/

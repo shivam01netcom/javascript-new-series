5565ddb8c5fe888dfb73dd0ef33f3138a687163c
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-23 09:12:46 +0000

    03_conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,58 @@
+const num = "33"
+
+// string to number conversion 
+const newNumber = Number(num);
+const newNumberSecondWay = +num;
+const num2 = +"65avs";
+
+
+// console.log(typeof num);
+// console.log(typeof newNumber);
+// console.log(typeof newNumberSecondWay);
+// console.log(typeof num2);
+// console.log(num2);
+// console.log(newNumberSecondWay);
+
+// typeof NaN = number(remember)
+// "33" => 33
+// "33abd" => NaN
+//  true => 1; false => 0
+
+
+
+
+
+
+// number to string conversion
+
+const x = 23;
+const string = x+""; //hacks
+const secondWay = String(x);
+// console.log(typeof string);
+// console.log(string);
+// console.log(typeof secondWay);
+
+// boolean conversion
+const i = 1;
+const booleanConversion = Boolean(i);
+//console.log(booleanConversion); // true
+
+// 1 => true;  0 => false
+// "" => false; "shivam"=> true; " " => true;
+
+// ***************** Operations ******************
+
+//console.log(2/3);
+//console.log(2%3);// give remender 2
+
+const str1 = "sk";
+const str2 = "kumar";
+const str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1"+2);//12
+// console.log("1"+"2");//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"3");//33
+// console.log(1+"2"+6);//126

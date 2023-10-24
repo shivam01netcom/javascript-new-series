3ce7ca68c35bd943f8706e458f366c3bdebf204d
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-24 11:13:40 +0000

    dataTypes-summary.js

---
+++ dataTypes-summary.js
@@ -0,0 +1,9 @@
+// Primitive(call by value)
+//7 types : String, Number, Boolean, Undefined, null, BigInt, Symbol  
+
+// Referance (Non Primitive)(call by reference)
+//Array, Objects, Functions
+
+// JavaScript statically typed or dynamically typed language 
+
+// JavaScript is a dynamically typed language. This means that in JavaScript, the data types of variables are not explicitly declared and are not fixed. Instead, the data type of a variable is determined at runtime, based on the type of value it currently holds.

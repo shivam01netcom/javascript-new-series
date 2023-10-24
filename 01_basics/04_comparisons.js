6955ad87b7bfebbe023edc3034f0bad6f28c0e11
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-24 10:59:35 +0000

    04_comparisons.js

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -9,13 +9,27 @@
 // important "one of this difference between type script and javascript"
 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null < 0);
-console.log(null !=0);
-console.log(null >= 0);
-console.log(null <= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null !=0);
+// console.log(null >= 0);
+// console.log(null <= 0);
 
 /*The reason is that an equality check == and comparison < > <= >= work differently 
 Comparisons convert null to a number, treating it as 0.
 That's why null>=0,null<=0 is true and null > 0,null < 0 is false. */
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined <=0);
+
+// undefined ke case me aisa kuch nahi hota null jaisa 
+
+// but always avoid this type of conversion , always try to compare similar data type variables
+
+//===
+// console.log("2"===2);
+// console.log(2===2);

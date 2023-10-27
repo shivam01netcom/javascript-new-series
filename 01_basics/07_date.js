502ae605567dbddd203f8fcb16e547872967a499
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-27 08:38:39 +0000

    07_dates chnages

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -8,3 +8,8 @@ console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toJSON());
+
+console.log(typeof myDate);//object
+
+let myNewDateCreated = new Date(2023,0,24);//month start from zero
+console.log(myNewDateCreated.toDateString());

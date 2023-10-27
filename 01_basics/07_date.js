96c64b44d131924fde21c530deb49baf5ea5a73b
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-10-27 08:35:34 +0000

    07_dates

---
+++ 01_basics/07_date.js
@@ -0,0 +1,10 @@
+
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toJSON());

cfb9f350b670378b6b69536c3dd4adb44d33e88a
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-11-13 07:02:03 +0000

    01_function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,80 @@
+  function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+    console.log("A");
+    console.log("M");
+  }
+
+  sayMyName;//referance of the function
+  sayMyName();// aise karne se excute ho jayega function
+
+
+function addTwoNumber(num1,num2){
+    num1+num2;
+}
+
+const result = addTwoNumber(5,7);
+//console.log("Result: ",result);
+
+
+
+
+
+  function addTwoNumbers(num1,num2){
+         // let result = num1 + num2;
+          // return result;
+              return num1+num2;
+             console.log("shivam");
+  }
+const result2 = addTwoNumbers(3,5);
+//console.log("Result2: ",result2);
+
+
+function loginUserMessage(username){
+    // if(username != undefined){
+    // return `${username} just logged in`
+    // }else{
+    // return "please logged in";
+    // }
+
+    // if(username === undefined){
+    //     console.log("Please enter a user name");
+    //     return;
+    // }
+    // return `${username} just logged in`
+
+    if(username){
+        return `${username} just logged in`
+    }
+    console.log("Please enter a user name");
+}
+
+// console.log(loginUserMessage("shivam"));
+// console.log(loginUserMessage());
+
+
+function loginUserMessage3(username = "sam"){
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage3("shivamKeshari"));
+// console.log(loginUserMessage3());
+
+//rest operator
+//note point rest and spread operator dono ko ... se hi karte h bas dono ke use cases alag alag h important
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200,600,300));//[ 200, 600, 300 ]
+
+//console.log(calculateCartPrice(200,600,300,8000));//[ 200, 600, 300,8000 ]
+
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,600,300,700,1000));

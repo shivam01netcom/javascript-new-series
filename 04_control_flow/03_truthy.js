48d4ccc3c85465c19a2f3add9a8b856984befb29
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-11-15 08:39:43 +0000

    truthy values

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,13 @@
+const userEmail = "sk@gmail.com";
+
+if(userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Don't have user email");
+}
+
+//falsy value
+//false , 0 , -0, 0n , "" , null, undefined, NaN
+
+//truly value
+// [],true, aur sare values falsy values ko chorrkar

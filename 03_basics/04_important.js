c04c19373e1f9e7314b87b2bc2920e14b3fa086a
Shivam Kumar <139260517+shivam01netcom@users.noreply.github.com>
2023-11-15 06:49:20 +0000

    very important

---
+++ 03_basics/04_important.js
@@ -0,0 +1,5 @@
+//How does javascript works behind the scene
+// must watch , jab bhi lage bhul rahe h internally kaise kam hota h 
+// that means globle execution context ,memory Creation, memory execution 
+
+// in sab ke liye watch 5:55 se 6:21 tak ka video 
